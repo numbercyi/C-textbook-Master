e6fba4fc942c773a9bbbaf11dd29cbef438a7170
numbercyi <59600210+numbercyi@users.noreply.github.com>
2021-12-03 21:38:04 +0900

    Add files via upload

---
+++ HelloWorld2/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("안녕하세요");
+        Console.ReadLine();
+    }
+}
